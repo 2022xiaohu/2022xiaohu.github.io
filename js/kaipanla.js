b13ebd59366f728a148faaf9d5ab12137b353fdb
studyNowHa <41898826+studyNowHa@users.noreply.github.com>
2023-02-07 23:07:14 +0800

    增加次日涨幅显示

--- js/kaipanla.js
+++ js/kaipanla.js
@@ -55,3 +55,43 @@
         get_limit1_data('fivePlate', '2', 'limit5', '更高')
         get_limit2_data('fourBoardStocksYesterday', '2', 'limit55', '未涨停的昨日四板个股')
     }
+
+    // 获取股票历史涨幅
+    function stocksHistory(date) {
+        var m_data = []
+        $.ajax({
+            url: 'https://apphq.longhuvip.com/w1/api/index.php',
+            type: 'POST',
+            data: {
+                'Order': '1',
+                'a': 'RealRankingInfo_W8',
+                'st': '10000',
+                'apiv': 'w31',
+                'Type': '8',
+                'c': 'NewStockRanking',
+                'PhoneOSNew': '1',
+                'DeviceID': 'ffffffff-fade-f24e-f9d3-785f00000000',
+                'index': '0',
+                'Date': date,
+                'PidType': '5',
+                'VerSion': '5.7.0.14',
+                'Isst': '0',
+                'FilterMotherboard': '0',
+                'Filter': '0',
+                'Ratio': '6',
+                'FilterTIB': '0',
+                'FilterGem': '0',
+            },
+            async: true,
+            dataType: 'text',
+            // contentType: 'application/json; charset=UTF-8',
+            success: function (data) {
+                var data = eval('(' + data + ')')['info'][0];//json格式化数据
+                console.log(data);
+            },
+            error: function (msg) {
+                console.log(msg);
+            }
+        })
+
+    };
