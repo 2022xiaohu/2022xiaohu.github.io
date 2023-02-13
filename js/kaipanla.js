a489ca9e05ec347eeaaa295a551ac355dbf5475d
Administrator <xxx>
2023-02-13 16:25:39 +0800

    增加个股涨幅历史查询

--- js/kaipanla.js
+++ js/kaipanla.js
@@ -1,5 +1,48 @@
 // 获取股票历史涨幅
-function stocksHistory(date, callback) {
+function hisStockRanking(date, callback) {
+    var m_data = []
+    $.ajax({
+        url: 'https://apphis.longhuvip.com/w1/api/index.php',
+        type: 'POST',
+        data: {
+            'Order': '1',
+            'a': 'HisRankingInfo_W8',
+            'st': '60',
+            'c': 'HisStockRanking',
+            'PhoneOSNew': '1',
+            'DeviceID': 'ffffffff-fade-f24e-f9d3-785f00000000',
+            'Isst': '0',
+            'index': '0',
+            'Date': date,
+            'apiv': 'w31',
+            'Type': '8',
+            'FilterMotherboard': '0',
+            'Filter': '0',
+            'Ratio': '6',
+            'FilterTIB': '0',
+            'FilterGem': '0',
+            // 'PidType': '5',
+            // 'VerSion': '5.7.0.14',
+            
+
+        },
+        async: true,
+        dataType: 'text',
+        // contentType: 'application/json; charset=UTF-8',
+        success: function (data) {
+            var data = eval('(' + data + ')');//json格式化数据
+            console.log(data);
+            callback(data)
+        },
+        error: function (msg) {
+            console.log(msg);
+        }
+    })
+
+};
+
+// 获取股票历史涨幅
+function newStockRanking(date, callback) {
     var m_data = []
     $.ajax({
         url: 'https://apphq.longhuvip.com/w1/api/index.php',
@@ -38,6 +81,7 @@ function stocksHistory(date, callback) {
     })
 
 };
+
 
 // 炸板
     function get_broke() {
